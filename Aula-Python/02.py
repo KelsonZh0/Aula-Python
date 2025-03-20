dd7230d1c00b1563599649b7c20dcc1d89ef7924
KelsonZh0 <kel180498@gmail.com>
2025-03-19 22:06:17 -0300

    exercico python finalizado

--- Aula-Python/02.py
+++ Aula-Python/02.py
@@ -1,14 +1,12 @@
-# Pergunte ao usuário por 2 números (x e y)
+'''# 2. Programa "calculadora" (com operações fixas por enquanto).
+Peça a entrada de dois números.
+    (a) Mostre a soma entre os números fornecidos
+    (b) Mostre outras três operações à sua escolha
+'''
 x = int(input(' Qual o Valor de X = '))
 y = int(input(' Qual o Valor de Y = '))
 
-#(a) Mostre a soma x+y
-print('A soma de X+Y é =' ,x+y)
-
-#(b) Mostre mais três operações a sua escolha 
-x = int(input(' Qual o Valor de X = '))
-y = int(input(' Qual o Valor de Y = '))
-
-print(x*x/y)
-print(x**x-y)
-print(x-y)
+print('A soma de x+y= ',x+y)
+print('A multiplicação x*y= ',x*y)
+print('A divisão de x/y= ',x/y)
+print('A subtração de x-y= ',x-y)
